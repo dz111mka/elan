f6ac64f1e64d84eebf31a7e231581ebb29ea0be6
Dmitry Chepikov <chepikov.d.u@gmail.com>
2024-04-05 22:36:37 +0300

    fix version bug

--- src/main/java/ru/chepikov/elan/config/BotConfig.java
+++ src/main/java/ru/chepikov/elan/config/BotConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Value;
 
+
 @Configuration
 @Data
 @PropertySource("classpath:application.yml")
